4a98726fd7da2233c622dae1a9c32a9e62b72960
alexia421 <alexiavisenica@gmail.com>
2026-09-18 09:14:30 +0300

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ServoTestConfig.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.Util;
-import com.acmerobotics.dashboard.config.Config;
-
-@Config
-public class ServoTestConfig {
-
-    public static double servoPos = 0.5;
-}
